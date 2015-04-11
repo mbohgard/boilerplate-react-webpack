5c232d67efa14ffe89ef65a7e44d98545024cb7a
Martin Bohgard <martin.bohgard@gmail.com>
2015-04-11 12:00:01 +0200

    resetting actions and store

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -1,15 +1,5 @@
 var Reflux = require('reflux');
 
 module.exports = Reflux.createActions([
-    "getAll"                // get all services
-  , "getAllOptions"         // get all providers and locations (from dummy json)
-  , "getService"            // get a single service
-  , "createService"         // create new service with placement
-  , "updateService"         // update existing service
-  , "deleteService"         // kill service
 
-  , "toggleNewService"      // toggle new service forms
-  , "addService"            // add service, client only, api call
-
-  , "clearError"            // clear errors
 ]);
